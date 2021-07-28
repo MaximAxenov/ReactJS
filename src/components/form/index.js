95b17f314fb0b4f395f24ce56a15ea207da5b737
МаксимАксёнов <aksenovmax777@gmail.com>
2021-07-28 13:28:11 +0300

    lesson-1
    lesson-1

---
+++ src/components/form/index.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import { AUTHORS, AUTHORs } from "../../constants";
+import '../form/index.css'
+
+export const Form = ({onSendMessage}) => {
+    const [value, setValue]= useState('');
+
+    const handleChange=(e)=>{
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        onSendMessage({
+            author: AUTHORS.human,
+            id: Date.now(),
+            text: value,
+        });
+        setValue('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input className="input" type="text" value={value} onChange={handleChange}/>
+            <input className="butt" type="submit"/>
+        </form>
+    )
+}
