cb0e38ccb3e3456e70344eb7a6f53c085382e46d
МаксимАксёнов <aksenovmax777@gmail.com>
2021-08-25 11:14:52 +0300

    lesson-10
    lesson-10

--- src/components/ChatList/ChatItem.js
+++ src/components/ChatList/ChatItem.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { ListItem } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { ListItem } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteChatWithFB } from "../../store/chats/actions";
 import "../ChatList/ChatList.css"
 
 export const ChatItem = ({ id, name, onDelete }) => {
+  const dispatch = useDispatch();
+
   const handleDelete = () => {
-    onDelete(id);
-  }
+    dispatch(deleteChatWithFB(id));
+  };
+  
   return (
     <ListItem>
       <Link className="chat" to={`/home/${id}`}>{name}</Link>
-      <div onClick={handleDelete}>&#10006;</div>
+      <div onClick={handleDelete}>DELETE</div>
     </ListItem>
   );
-}
+};
--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -1,15 +1,13 @@
 import React, { useContext } from "react";
 import { List, ListItem } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { ThemeContext } from "../../utils/ThemeContext";
-import { withThemeContext } from "../Message";
 import { AddChat } from "./AddChat";
 import { ChatItem } from "./ChatItem";
 
-export const ChatList = ({ chats, theme, onDeleteChat, onAddChat }) => {
+export const ChatList = ({ chats, onDeleteChat, onAddChat }) => {
   return (
     <>
-      <List>
+    <List>
         {Object.values(chats).map((c) => (
           <ChatItem
             name={c.name}
